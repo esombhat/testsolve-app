1b0192e99f5dfe1934c87df5b292dd120ed243c5
Eshaan Sombhatta <155382057+esombhat@users.noreply.github.com>
2025-08-04 01:11:30 +0000

    next commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Welcome to the VMT Testsolve app.")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "To enter, please login."
 )
+st.button("Login")
